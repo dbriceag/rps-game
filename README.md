53fb321ddef81899ff2f519d8914ae5077d1b1ca
Denys Brichak <brichak@gmail.com>
2017-04-18 06:38:16 +0200

    gradle build added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rps-game
 rock paper scissors game
+
+To build, execute ./gradlew build or gradlew.exe build
+To create/update eclipes project, execute ./gradlew cleanEclipse eclipse of gradlew.exe cleanEclipse eclipse
+
+
--- rps-game/src/main/java/com/brichak/assignments/rpsgame/ui/ConsolePlayer.java
+++ rps-game/src/main/java/com/brichak/assignments/rpsgame/ui/ConsolePlayer.java
@@ -2,7 +2,6 @@ package com.brichak.assignments.rpsgame.ui;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 import com.brichak.assignments.rpsgame.core.Choice;
 import com.brichak.assignments.rpsgame.core.Player;
@@ -20,15 +19,13 @@ class ConsolePlayer implements Player {
 	@Override
 	public Choice play() {
 		String choice;
-		@SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
 		do {
 			System.out.println("Please select: ");
 			System.out.println("1 - Rock");
 			System.out.println("2 - Paper");
 			System.out.println("3 - Scissors");	
-			
-		 	choice = sc.nextLine();
+	 
+			choice = System.console().readLine();
 		} while (!validOptions.keySet().contains(choice));
 		
 		return validOptions.get(choice);
